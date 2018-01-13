aa471ba2063afdb997f4f1b2a527386d555b1d99
EricaDD <ericadd@dds-macbook-pro.local>
2018-01-13 23:18:01 +0800

    创建了Person类

---
+++ GITHUB_TEST/GITHUB_TEST/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  GITHUB_TEST
+//
+//  Created by EricaDD on 2018/1/13.
+//  Copyright © 2018年 EricaDD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
