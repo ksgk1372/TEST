6cb05848fe1b96ecb4cf22cdb89ad2187dd3de20
EricaDD <ericadd@dds-macbook-pro.local>
2018-01-13 23:30:00 +0800

    1111111111

---
+++ GITHUB_TEST/GITHUB_TEST/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  GITHUB_TEST
+//
+//  Created by EricaDD on 2018/1/13.
+//  Copyright © 2018年 EricaDD. All rights reserved.
+//
+
+#import "Person.h"
+
+@interface Student : Person
+
+@end
---
+++ GITHUB_TEST/GITHUB_TEST/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  GITHUB_TEST
+//
+//  Created by EricaDD on 2018/1/13.
+//  Copyright © 2018年 EricaDD. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
